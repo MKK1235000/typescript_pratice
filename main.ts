e6b44e5cbb6ab8e8041755e317855c040fc19c17
MKK1235000 <khalidkhn@gmail.com>
2024-02-21 08:22:45 +0500

    first commit

---
+++ main.ts
@@ -0,0 +1,21 @@
+console.log("Hello World!");
+console.log("Hello Khalid!");
+let personname = "m.khalid khan";
+console.log(personname);
+let PERSONNAME = "M.KHALID KAHN";
+console.log(PERSONNAME);
+let PersonName = "M.Khalid Khan";
+console.log(PersonName);
+let quote:string ="Ghani Khan once said, Pashtun is not merely a race but in fact a state of mind there is a Pashtun lying inside every man who at times wakes up and overpowers him. The Pashtuns are rain-sown wheat"
+console.log(quote);
+let AuthorName = "Ghani Khan";
+let Quote:string="Pashtun is not merely a race but in fact a state of mind there is a Pashtun lying inside every man who at times wakes up and overpowers him. The Pashtuns are rain-sown wheat";
+console.log(AuthorName+"once said,"+Quote);
+let orginal_name: string = " \t khalid \n";
+console.log(orginal_name,"Orginal_Name");
+let striped_name:string= orginal_name.trim();
+console.log(striped_name,"Striped Name");
+console.log("Addition",4+2);
+console.log("Subtraction",4-2);
+console.log("Multiplication",4*2);
+console.log("Divison",4/2);
